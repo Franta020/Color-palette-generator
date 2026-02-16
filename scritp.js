581529375e2548d444d4a7bb8079690c455908d1
Franta020 <oofranta@gmail.com>
2026-02-16 21:44:12 +0100

    funkční generátor náhodných barev

--- scritp.js
+++ scritp.js
@@ -1,4 +1,32 @@
 const generateButton = $(".generate-btn");
-const color = $(".color");
-const colorValue = $(".color-value");
+const color = document.querySelectorAll(".color");
+const colorValue = Array.from($(".color-value"));
 const copyButton = $(".copy-btn");
+const palleteContainer = $(".palette-container");
+
+generateButton.on("click", generatePalette);
+
+function generatePalette() {
+  const colors = [];
+  for (let i = 0; i < 5; i++) {
+    colors.push(generateRandomColor());
+  }
+  updateColorUI(colors);
+}
+
+function generateRandomColor() {
+  const symbols = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += symbols[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function updateColorUI(colors) {
+  color.forEach((el, index) => {
+    el.style.background = colors[index];
+    const colorHex = el.querySelector(".color-value");
+    colorHex.innerHTML = colors[index];
+  });
+}
